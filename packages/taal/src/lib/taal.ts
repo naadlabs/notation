6b885860206d7e444ecabc66cc2626c9c04c0935
Indermohan Singh <indermohansinghk7@gmail.com>
2025-03-22 19:02:03 +0000

    chore(release): first version of taal package

--- packages/taal/src/lib/taal.ts
+++ packages/taal/src/lib/taal.ts
@@ -1,3 +1,17 @@
-export function taal(): string {
-  return 'taal';
+import { taals } from './taals.js';
+import type { Taal } from './types.js';
+
+function getTaals(): readonly Taal[] {
+  return taals;
 }
+
+function getTaalByName(name: string): Taal | undefined {
+  const filteredTaal = taals.filter((taal) => taal.name === name);
+  return filteredTaal.length ? filteredTaal[0] : undefined;
+}
+
+function getTaalByBeats(beats: number): readonly Taal[] {
+  return taals.filter((taal) => taal.beats === beats);
+}
+
+export { getTaals, getTaalByName, getTaalByBeats };
